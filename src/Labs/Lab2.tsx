a812381f559fb5d040835d7f3ed97ab39e92b441
KiswaK <160799425+KiswaK@users.noreply.github.com>
2024-10-01 21:55:51 -0400

    work now

--- src/Labs/Lab2.tsx
+++ src/Labs/Lab2.tsx
@@ -3,7 +3,7 @@ export default function Lab2(){
         <div>
             <h2>Lab2</h2>
             <h3>Exercise 2.1</h3>
-            <h3>Exercise Sai</h3>
+            <h3>Please work omg</h3>
         </div>
     )
 }
